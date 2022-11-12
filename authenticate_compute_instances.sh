f0e50535dd5552ee04b79b58e4acc1b7af89975c
Brennan Wilkes <brennan@codexwilkes.com>
2022-11-12 15:33:06 -0800

    fix: Eval

--- authenticate_compute_instances.sh
+++ authenticate_compute_instances.sh
@@ -2,10 +2,11 @@
 
 REPO="$1"
 DATA_FILE="~/$REPO/authDataFile.txt"
+DATA=$( eval cat $DATA_FILE )
 
 while IFS= read -r worker; do
 	IP=$( echo "$worker" | cut -d',' -f1 )
 	USER=$( echo "$worker" | cut -d',' -f2 )
 	KEY=$( echo "$worker" | cut -d',' -f3 )
 	ssh -i "$KEY" "$USER@$IP" "cd ~/$REPO && gh auth status || gh auth login"
-done < "$DATA_FILE"
+done <<< "$DATA"
