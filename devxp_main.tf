5e01cfc60ae838f63478829a06aade680af9bdea
Brennan Wilkes <brennan@codexwilkes.com>
2022-11-12 15:43:41 -0800

    fix: SSH adjustments

--- devxp_main.tf
+++ devxp_main.tf
@@ -323,6 +323,12 @@ resource "aws_security_group" "devxp_security_group" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  egress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   ingress {
     from_port   = 80
     to_port     = 80
