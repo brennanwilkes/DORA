2adc9aed91e587e7cf6de0dae69a93f07762fdf5
Brennan Wilkes <brennan@codexwilkes.com>
2022-11-12 16:45:27 -0800

    fix: Install stuff

--- start_compute_instances.sh
+++ start_compute_instances.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 REPO="$1"
-USER="$2"
-IP="$3"
-KEY="$4"
+REMOTE_USER="$2"
+REMOTE_IP="$3"
+REMOTE_KEY="$4"
 DATA_FILE="~/$REPO/authDataFile.txt"
 DATA=$( eval cat $DATA_FILE )
 
@@ -12,7 +12,6 @@ while IFS="\n" read -r worker; do
 	IP=$( echo "$worker" | cut -d',' -f1 )
 	USER=$( echo "$worker" | cut -d',' -f2 )
 	KEY=$( echo "$worker" | cut -d',' -f3 )
-	echo ssh -o StrictHostKeyChecking=no -i "$KEY" "$USER@$IP" "cd ~/$REPO && touch SCHEDULER_REQUESTED_START && nohup ./worker_study.sh '$i' '$USER' '$IP' '$KEY' >~/worker.out 2>~/worker.err </dev/null &"
-	ssh -o StrictHostKeyChecking=no -i "$KEY" "$USER@$IP" "cd ~/$REPO && touch SCHEDULER_REQUESTED_START && nohup ./worker_study.sh '$i' '$USER' '$IP' '$KEY' >~/worker.out 2>~/worker.err </dev/null &"
+	ssh -o StrictHostKeyChecking=no -i "$KEY" -f "$USER@$IP" "cd ~/$REPO && touch SCHEDULER_REQUESTED_START && nohup ./worker_study.sh '$i' '$REMOTE_USER' '$REMOTE_IP' '$REMOTE_KEY' >~/worker.out 2>~/worker.err </dev/null & ; disown;"
 	i=$(( $i + 1 ))
 done <<< "$DATA"
