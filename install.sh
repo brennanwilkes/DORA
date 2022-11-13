68b1cf2d9a882f33cb6af4475cdbddd482979f5e
Brennan Wilkes <brennan@codexwilkes.com>
2022-11-12 16:50:12 -0800

    chore: Install script

--- install.sh
+++ install.sh
@@ -3,8 +3,9 @@
 #Install Node
 node -v || {
 	curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
-	source ~/.bashrc
+	source $HOME/.bashrc
 	export NVM_DIR="$HOME/.nvm"
+	source $HOME/.nvm/nvm.sh
 	[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 	[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 	nvm install v18.3.0
