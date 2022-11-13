746908fe48e87c24e5b67f50326f8b64a5ae941a
Brennan Wilkes <brennan@codexwilkes.com>
2022-11-12 17:42:50 -0800

    chore: Result combiner adjustments

--- add_to_result.js
+++ add_to_result.js
@@ -4,7 +4,9 @@ let output = JSON.parse(fs.readFileSync(process.argv[process.argv.length - 1]));
 
 for (let i = 2; i < process.argv.length - 1; i++){
 	const json = JSON.parse(fs.readFileSync(process.argv[i]));
-	output.results = [...(output.results.filter(r => r.repo !== json.result.repo)), json.result];
+	output.results = [...(output.results.filter(r => {
+		return (json.result ? [json.result] : json.results).map(rr => rr.repo).indexOf(r.repo) === -1;
+	})), ...(json.result ? [json.result] : json.results)];
 }
 
 fs.writeFileSync(process.argv[process.argv.length - 1], JSON.stringify(output, null, 4));
--- result_combiner.js
+++ result_combiner.js
@@ -7,7 +7,7 @@ for (let i = 2; i < process.argv.length - 1; i++){
 	const json = JSON.parse(fs.readFileSync(process.argv[i]));
 	output.start = json.start;
 	output.end = json.end;
-	output.results = [...(output.results), json.result];
+	output.results = [...(output.results), ...(json.result ? [json.result] : json.results)];
 }
 
 fs.writeFileSync(process.argv[process.argv.length - 1], JSON.stringify(output, null, 4));
