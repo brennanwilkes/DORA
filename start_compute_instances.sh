9e62e623e603a6f0bb41dcd6a3bd7b0f75bc641a
Brennan Wilkes <brennan@codexwilkes.com>
2022-11-12 16:30:11 -0800

    chore: Debugging

--- start_compute_instances.sh
+++ start_compute_instances.sh
@@ -7,13 +7,11 @@ KEY="$4"
 DATA_FILE="~/$REPO/authDataFile.txt"
 DATA=$( eval cat $DATA_FILE )
 
-echo "Please run:"
-
 i=0
 while IFS="\n" read -r worker; do
 	IP=$( echo "$worker" | cut -d',' -f1 )
 	USER=$( echo "$worker" | cut -d',' -f2 )
 	KEY=$( echo "$worker" | cut -d',' -f3 )
-	ssh -o StrictHostKeyChecking=no -i "$KEY" "$USER@$IP" "cd ~/$REPO && nohup ./worker_study.sh '$i' '$USER' '$IP' '$KEY' >/dev/null 2>/dev/null </dev/null &"
+	ssh -o StrictHostKeyChecking=no -i "$KEY" "$USER@$IP" "cd ~/$REPO && touch SCHEDULER_REQUESTED_START && nohup ./worker_study.sh '$i' '$USER' '$IP' '$KEY' >/dev/null 2>/dev/null </dev/null &"
 	i=$(( $i + 1 ))
 done <<< "$DATA"
--- worker_study.sh
+++ worker_study.sh
@@ -5,7 +5,11 @@ USER="$2"
 REMOTE="$3"
 KEY="$4"
 
+touch I_HAVE_STARTED
+
 ./study.sh config.json > stdout.txt
 scp -i "$KEY" stdout.txt "$USER@$REMOTE:~/$WORKER_ID-stdout.txt"
 scp -i "$KEY" log "$USER@$REMOTE:~/$WORKER_ID-log"
 scp -i "$KEY" results.json "$USER@$REMOTE:~/$WORKER_ID-results.json"
+
+touch I_HAVE_COMPLETED
