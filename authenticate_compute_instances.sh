574a5273cff9529e7500020951dccf06fb4555ed
Brennan Wilkes <brennan@codexwilkes.com>
2022-11-12 16:12:55 -0800

    chore: Switch to manual auth

--- authenticate_compute_instances.sh
+++ authenticate_compute_instances.sh
@@ -4,10 +4,12 @@ REPO="$1"
 DATA_FILE="~/$REPO/authDataFile.txt"
 DATA=$( eval cat $DATA_FILE )
 
+echo "Please run:"
+
 while IFS="\n" read -r worker; do
 	IP=$( echo "$worker" | cut -d',' -f1 )
 	USER=$( echo "$worker" | cut -d',' -f2 )
 	KEY=$( echo "$worker" | cut -d',' -f3 )
-	echo ssh -o StrictHostKeyChecking=no -i "$KEY" "$USER@$IP" "cd ~/$REPO && gh auth status || gh auth login"
-	ssh -o StrictHostKeyChecking=no -i "$KEY" "$USER@$IP" "cd ~/$REPO && gh auth status || gh auth login"
+	echo ssh -o StrictHostKeyChecking=no -i "$KEY" "$USER@$IP" "'cd ~/$REPO && gh auth status || gh auth login'"
+	# ssh -o StrictHostKeyChecking=no -i "$KEY" "$USER@$IP" "cd ~/$REPO && gh auth status || gh auth login"
 done <<< "$DATA"
