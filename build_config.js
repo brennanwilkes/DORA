5946e0d7e49265c7bdc3de8f194c8ce6f39edd56
Brennan Wilkes <brennan@codexwilkes.com>
2022-11-14 22:00:42 -0800

    chore: Prep for v1 mine

---
+++ build_config.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+
+const data = fs.readFileSync(process.argv[2]).toString().split("\n");
+const output = {
+	name: "",
+	start: "",
+	end: "",
+	results: "",
+	repos: data.filter(r => r.length > 0).map(r => r.split(",")).sort((a,b) => parseInt(a[2]) - parseInt(b[2])).map(r => {
+		return {
+			id: r[0],
+			deployment: 1,
+			failures: {type: 1}
+		};
+	}),
+	scheduler: {},
+	workers: []
+};
+console.log(JSON.stringify(output, null, 4))
